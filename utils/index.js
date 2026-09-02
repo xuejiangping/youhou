2a3276313dfe99cb96459bff324ea75c37749ac7
xuejiangping <1127523185@qq.com>
2026-09-03 05:47:38 +0800

    初始化

---
+++ utils/index.js
@@ -0,0 +1,32 @@
+
+
+export function request_yh({
+  method,
+  url,
+  timeout
+}) {
+  return new Promise((resolve,reject) => {
+    if (typeof GM_xmlhttpRequest !== 'undefined') {
+      GM_xmlhttpRequest({
+        method,
+        url,
+        timeout,
+        onload: function (res) {
+          resolve(res)
+        },
+        onerror: function (err) {
+          reject(err)
+        },
+        ontimeout: function () {
+          reject('timeout:' + timeout)
+        }
+      });
+    } else {
+      return fetch({
+        method,url,
+      })
+    }
+  })
+
+
+}
