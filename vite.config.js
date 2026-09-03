f27cd51df6493b5fbe4be5cd8a0f5e963324e8d1
xuejiangping <1127523185@qq.com>
2026-09-03 08:45:01 +0800

    build utils

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,21 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(import.meta.dirname,'./utils/index.js'),
-      name: 'utils',
+      name: 'YHUtils',
       // 将添加适当的扩展名后缀
-      fileName: 'youhou-utils',
+      fileName: 'yh-utils',
 
 
     },
     rolldownOptions: {
       // 确保外部化处理那些
       // 你不想打包进库的依赖
-      external: ['vue'],
+      external: [],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖
         // 提供一个全局变量
         globals: {
-          vue: 'youhou',
+
         },
       },
     },
